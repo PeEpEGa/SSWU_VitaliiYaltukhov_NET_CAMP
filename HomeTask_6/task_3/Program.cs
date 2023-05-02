7083423d2e8e1e5e5a52b4887c5996733851cd9c
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-02 15:21:51 +0300

    Update Program.cs

--- HomeTask_6/task_3/Program.cs
+++ HomeTask_6/task_3/Program.cs
@@ -1,5 +1,5 @@
 ﻿using task2._1_sigma_;
-
+// Cумарний бал - 95.
 Console.WriteLine("Unique words:");
 foreach (var item in StringHelper.FindUniqueWords("   pizza  fd  i f  u   \n q3dfw3 fe  q3fdfw3  fwef afd  fwef pizza  pizza123 pizza ug"))
 {
